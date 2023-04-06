df12081ba77a2ecb7d82e5360672fa958929bbe9
hansilly14 <124852046+hansilly14@users.noreply.github.com>
2023-04-06 09:51:51 +0700

    Update CTDL&GT_Queue_2.cpp

--- CTDL&GT_Queue_2.cpp
+++ CTDL&GT_Queue_2.cpp
@@ -9,7 +9,6 @@ void display();
 int items[SIZE], front = -1, rear = -1;
 
 int main() {
-  //deQueue is not possible on empty queue
   deQueue();
 
   //enQueue 5 elements
@@ -18,16 +17,11 @@ int main() {
   enQueue(3);
   enQueue(4);
   enQueue(5);
-
-  // 6th element can't be added to because the queue is full
   enQueue(6);
 
   display();
-
-  //deQueue removes element entered first i.e. 1
   deQueue();
 
-  //Now we have just 4 elements
   display();
 
   return 0;
