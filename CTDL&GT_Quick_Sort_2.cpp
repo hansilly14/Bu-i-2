0a21dff73bdc9f130fa72a53b8bdfe9adc640e33
hansilly14 <124852046+hansilly14@users.noreply.github.com>
2023-04-06 09:51:09 +0700

    Update CTDL&GT_Quick_Sort_2.cpp

--- CTDL&GT_Quick_Sort_2.cpp
+++ CTDL&GT_Quick_Sort_2.cpp
@@ -1,15 +1,11 @@
 https://blog.luyencode.net/thuat-toan-sap-xep-quick-sort/
 #include<stdio.h>
- 
- 
 void swap(int &a, int &b)
 {
     int t = a;
     a = b;
     b = t;
 }
- 
- 
 int partition (int arr[], int low, int high)
 {
     int pivot = arr[high];    // pivot
@@ -26,23 +22,18 @@ int partition (int arr[], int low, int high)
     swap(arr[left], arr[high]);
     return left;
 }
- 
-/* Hàm thực hiện giải thuật quick sort */
+
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
-        /* pi là chỉ số nơi phần tử này đã đứng đúng vị trí
-         và là phần tử chia mảng làm 2 mảng con trái & phải */
         int pi = partition(arr, low, high);
  
-        // Gọi đệ quy sắp xếp 2 mảng con trái và phải
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
  
-/* Hàm xuất mảng */
 void printArray(int arr[], int size)
 {
     int i;
