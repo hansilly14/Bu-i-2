866304e4810aff534331600ec6d2a5ea405620c6
hansilly14 <124852046+hansilly14@users.noreply.github.com>
2023-04-06 08:17:46 +0700

    Create CTDL&GT_Heap_Sort_1.cpp

---
+++ CTDL&GT_Heap_Sort_1.cpp
@@ -0,0 +1,17 @@
+https://cafedev.vn/thuat-toan-heapsort-gioi-thieu-chi-tiet-va-code-vi-du-tren-nhieu-ngon-ngu-lap-trinh/
+void heapSort(int arr[], int n) 
+{ 
+    // Build heap (rearrange array) 
+    for (int i = n / 2 - 1; i >= 0; i--) 
+        heapify(arr, n, i); 
+  
+    // One by one extract an element from heap 
+    for (int i=n-1; i>0; i--) 
+    { 
+        // Move current root to end 
+        swap(arr[0], arr[i]); 
+  
+        // call max heapify on the reduced heap 
+        heapify(arr, i, 0); 
+    } 
+} 
