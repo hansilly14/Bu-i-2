59dbea4c9b5dc51900d7ec24835e5e22b6502537
hansilly14 <124852046+hansilly14@users.noreply.github.com>
2023-04-06 09:06:47 +0700

    Create CTDL&GT_Queue_1.cpp

---
+++ CTDL&GT_Queue_1.cpp
@@ -0,0 +1,36 @@
+https://tek4.vn/khoa-hoc/cau-truc-du-lieu-va-thuat-toan/hang-doi
+void enQueue(int value) {
+  if (rear == SIZE - 1)
+    printf("\nQueue is Full!!");
+  else {
+    if (front == -1)
+      front = 0;
+    rear++;
+    items[rear] = value;
+    printf("\nInserted -> %d", value);
+  }
+}
+
+void deQueue() {
+  if (front == -1)
+    printf("\nQueue is Empty!!");
+  else {
+    printf("\nDeleted : %d", items[front]);
+    front++;
+    if (front > rear)
+      front = rear = -1;
+  }
+}
+
+// Function to print the queue
+void display() {
+  if (rear == -1)
+    printf("\nQueue is Empty!!!");
+  else {
+    int i;
+    printf("\nQueue elements are:\n");
+    for (i = front; i <= rear; i++)
+      printf("%d  ", items[i]);
+  }
+  printf("\n");
+}
