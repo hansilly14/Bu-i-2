502d3c688eb05bb9d5756a6c7d730f60ccf1ae4b
hansilly14 <124852046+hansilly14@users.noreply.github.com>
2023-04-06 08:29:18 +0700

    Update CTDL&GT_Stack_1.cpp

--- CTDL&GT_Stack_1.cpp
+++ CTDL&GT_Stack_1.cpp
@@ -1,8 +1,36 @@
-Silde bài giảng chương 3 
-  Void sxdoicho(int a[], int n)
+https://tek4.vn/khoa-hoc/cau-truc-du-lieu-va-thuat-toan/ngan-xep
+int isFull(struct Stack* stack)
 {
-	for(int i=0; i<n-1;i++)
-		for(int j=i+1;j<n; j++)
- 			if(a[i]>a[j])
-			hoanvi(a[i],a[j]);
+	return stack->top == stack->capacity - 1;
+}
+
+// Stack is empty when top is equal to -1
+int isEmpty(struct Stack* stack)
+{
+	return stack->top == -1;
+}
+
+// Function to add an item to stack. It increases top by 1
+void push(struct Stack* stack, int item)
+{
+	if (isFull(stack))
+		return;
+	stack->array[++stack->top] = item;
+	printf("%d pushed to stack\n", item);
+}
+
+// Function to remove an item from stack. It decreases top by 1
+int pop(struct Stack* stack)
+{
+	if (isEmpty(stack))
+		return INT_MIN;
+	return stack->array[stack->top--];
+}
+
+// Function to return the top from stack without removing it
+int peek(struct Stack* stack)
+{
+	if (isEmpty(stack))
+		return INT_MIN;
+	return stack->array[stack->top];
 }
