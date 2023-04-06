0f574ca2117d90867bb3c982f5494bd7be9b9f98
hansilly14 <124852046+hansilly14@users.noreply.github.com>
2023-04-06 09:48:33 +0700

    Update CTDL&GT_Stack_2.cpp

--- CTDL&GT_Stack_2.cpp
+++ CTDL&GT_Stack_2.cpp
@@ -1,18 +1,13 @@
 https://tek4.vn/khoa-hoc/cau-truc-du-lieu-va-thuat-toan/ngan-xep
-// C program for array implementation of stack
+
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-// A structure to represent a stack
 struct Stack {
 	int top;
 	unsigned capacity;
 	int* array;
-};
-
-// function to create a stack of given capacity. It initializes size of
-// stack as 0
+}
 struct Stack* createStack(unsigned capacity)
 {
 	struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
@@ -21,20 +16,14 @@ struct Stack* createStack(unsigned capacity)
 	stack->array = (int*)malloc(stack->capacity * sizeof(int));
 	return stack;
 }
-
-// Stack is full when top is equal to the last index
 int isFull(struct Stack* stack)
 {
 	return stack->top == stack->capacity - 1;
 }
-
-// Stack is empty when top is equal to -1
 int isEmpty(struct Stack* stack)
 {
 	return stack->top == -1;
 }
-
-// Function to add an item to stack. It increases top by 1
 void push(struct Stack* stack, int item)
 {
 	if (isFull(stack))
@@ -43,7 +32,6 @@ void push(struct Stack* stack, int item)
 	printf("%d pushed to stack\n", item);
 }
 
-// Function to remove an item from stack. It decreases top by 1
 int pop(struct Stack* stack)
 {
 	if (isEmpty(stack))
@@ -51,7 +39,7 @@ int pop(struct Stack* stack)
 	return stack->array[stack->top--];
 }
 
-// Function to return the top from stack without removing it
+
 int peek(struct Stack* stack)
 {
 	if (isEmpty(stack))
@@ -59,7 +47,6 @@ int peek(struct Stack* stack)
 	return stack->array[stack->top];
 }
 
-// Driver program to test above functions
 int main()
 {
 	struct Stack* stack = createStack(100);
